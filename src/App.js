ca35d3e71a3b9fbd58c570e6cca24567b82827b6
Yugyeong <uugyeong27@gmail.com>
2024-02-02 17:25:34 +0900

    Feat: add Book Detail Page & css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import MainPage from './components/MainPage'
+import MainPage from './components/MainPage';
+import BookDetailPage from './components/BookDetailPage';
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<MainPage />}></Route>
+                    <Route path="/" element={<BookDetailPage />}></Route>
                 </Routes>
             </BrowserRouter>
         </>
