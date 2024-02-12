fc2f58aaf653903e34c4a92c6512e1c04d2e3f1b
dayeon1201 <dydid1201@gmail.com>
2024-02-12 17:41:26 +0900

    feat: add Booklist test

---
+++ src/Booklist.js
@@ -0,0 +1,152 @@
+import React from "react";
+import { GradientBg1 } from "./icons/GradientBg1";
+import "./style.css";
+
+function Booklist() {
+  return (
+    <div className="index">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="overlap-group">
+            <GradientBg1 className="gradient-BG" />
+            <div className="rectangle" />
+            <div className="div" />
+            <div className="text-wrapper">재고</div>
+            <img
+              className="book"
+              alt="Book"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/k8s-book-1.png"
+            />
+            <div className="text-wrapper-2">쿠버네티스 인 액션</div>
+            <div className="text-wrapper-3">쿠우쿠우의 성공 비결</div>
+            <div className="text-wrapper-4">40,500원</div>
+            <div className="text-wrapper-5">11,000원</div>
+            <img
+              className="qoo-image"
+              alt="Qoo image"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/qoo-image-1.png"
+            />
+            <div className="text-wrapper-6">마르코 룩샤</div>
+            <div className="text-wrapper-7">사장님</div>
+            <img
+              className="line"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/line-1.svg"
+            />
+            <div className="rectangle-2" />
+            <div className="text-wrapper-8">대출</div>
+            <div className="rectangle-3" />
+            <div className="text-wrapper-9">재고</div>
+            <div className="rectangle-4" />
+            <div className="text-wrapper-10">대출</div>
+            <div className="rectangle-5" />
+            <div className="text-wrapper-11">재고</div>
+            <div className="rectangle-6" />
+            <div className="text-wrapper-12">대출</div>
+            <div className="rectangle-7" />
+            <div className="text-wrapper-13">재고</div>
+            <div className="rectangle-8" />
+            <div className="text-wrapper-14">대출</div>
+            <div className="rectangle-9" />
+            <div className="text-wrapper-15">재고</div>
+            <div className="rectangle-10" />
+            <div className="text-wrapper-16">재고</div>
+            <div className="rectangle-11" />
+            <div className="text-wrapper-17">대출</div>
+            <div className="group">
+              <img
+                className="chevron-left"
+                alt="Chevron left"
+                src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/chevron-left-2.svg"
+              />
+              <div className="text-wrapper-18">검색할 내용을 입력해주세요</div>
+              <img
+                className="img"
+                alt="Line"
+                src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/line-2.svg"
+              />
+              <div className="vector-wrapper">
+                <img
+                  className="vector"
+                  alt="Vector"
+                  src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/vector.svg"
+                />
+              </div>
+            </div>
+            <div className="text-wrapper-19">쿠우쿠우의 성공 비결</div>
+            <div className="text-wrapper-20">11,000원</div>
+            <img
+              className="ks-book"
+              alt="Book"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/k8s-book-1.png"
+            />
+            <div className="text-wrapper-21">사장님</div>
+            <img
+              className="line-2"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/line-3.svg"
+            />
+            <div className="text-wrapper-22">쿠우쿠우의 성공 비결</div>
+            <div className="text-wrapper-23">11,000원</div>
+            <img
+              className="qoo-image-2"
+              alt="Qoo image"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/qoo-image-1.png"
+            />
+            <div className="text-wrapper-24">사장님</div>
+            <img
+              className="line-3"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/line-3.svg"
+            />
+            <div className="text-wrapper-25">쿠우쿠우의 성공 비결</div>
+            <div className="text-wrapper-26">11,000원</div>
+            <img
+              className="qoo-image-3"
+              alt="Qoo image"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/qoo-image-1.png"
+            />
+            <div className="text-wrapper-27">사장님</div>
+            <img
+              className="line-4"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/line-3.svg"
+            />
+            <div className="text-wrapper-28">쿠우쿠우의 성공 비결</div>
+            <div className="text-wrapper-29">11,000원</div>
+            <img
+              className="qoo-image-4"
+              alt="Qoo image"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/qoo-image-1.png"
+            />
+            <div className="text-wrapper-30">사장님</div>
+            <img
+              className="line-5"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/line-3.svg"
+            />
+            <div className="text-wrapper-31">쿠우쿠우의 성공 비결</div>
+            <img
+              className="qoo-image-5"
+              alt="Qoo image"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/qoo-image-1.png"
+            />
+            <div className="text-wrapper-32">사장님</div>
+            <img
+              className="line-6"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/65c654a881257e46c59d2046/releases/65c654e3cea0d4810c2d3337/img/line-3.svg"
+            />
+            <div className="rectangle-12" />
+            <div className="text-wrapper-33">재고</div>
+            <div className="rectangle-13" />
+            <div className="text-wrapper-34">대출</div>
+          </div>
+          <div className="text-wrapper-35">11,000원</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Booklist;
