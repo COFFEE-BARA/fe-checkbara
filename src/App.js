615d241bf7851508e97d519050fa070d573081c6
dayeon1201 <dydid1201@gmail.com>
2024-02-20 18:59:37 +0900

    Style: css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ResultSection from './components/ResultSection.js';
 import MainPage from './components/MainPage.js';
 import RecommendInput from './components/RecommendInput';
 import RecommendChat from './components/RecommendChat';
-import { request } from './api/api.js'; //조정
+// import { request } from './api/api.js'; //조정
 import BookDetailPage from "./components/BookDetailPage"
 
 
@@ -18,7 +18,7 @@ function App() {
     const [keywords, setKeywords] = useState([]);
 
     const onSearch = async (keyword, pageNum) => {
-        const response = await request(keyword, pageNum); // 필요에 따라 적절한 방법으로 검색을 처리합니다.
+        // const response = await request(keyword, pageNum); // 필요에 따라 적절한 방법으로 검색을 처리합니다.
 
         let newKeywords = [...keywords];
         if (newKeywords.indexOf(keyword) !== -1) {
@@ -31,8 +31,8 @@ function App() {
         setKeywords(newKeywords);
         localStorage.setItem("keywords", JSON.stringify(newKeywords));
 
-        setData(response.documents);
-        setPageNum(pageNum);
+        // setData(response.documents);
+        // setPageNum(pageNum);
     };
 
     return (
