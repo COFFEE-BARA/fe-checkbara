09be4c76fca6b39f988fdfd901dd408cf1134e2d
Yugyeong <uugyeong27@gmail.com>
2024-02-22 13:58:27 +0900

    Feat: 마커 기능

--- src/components/Markup.js
+++ src/components/Markup.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { MapContainer, Marker, Popup } from "react-leaflet";
 // import { useLocation } from "react-router-dom";
 
@@ -6,73 +6,70 @@ import aladinIcon from "../images/aladin.png";
 import kyoboIcon from "../images/kyobo.png";
 import ypbookIcon from "../images/ypbooks.png";
 import libraryIcon from "../images/library.png";
+import { circleMarker } from "leaflet";
 
 function Markup({ path, result, currentMyLocation, map, isbn }) {
-    const [bookplaces, setBookplaces] = useState([]);
+
     // const [libraries, setLibraries] = useState([]);
     const [loading, setLoading] = useState(true); 
     let icon, name;
-    let array=[], marker;
+    let array=[];
+    const markerRef = useRef(null);
 
     useEffect(() => {
         if (result) {
-            for (var i = 0; i < 3; i++) {
-                if (result[i]) {
-                    for (var j = 0; j < result[i].length; j++) {
-                        let icon;
-                        switch (result[i][j].type) {
-                            case "교보문고":
-                                icon = kyoboIcon;
-                                break;
-                            case "알라딘":
-                                icon = aladinIcon;
-                                break;
-                            case "영풍문고":
-                                icon = ypbookIcon;
-                                break;
-                            default:
-                                icon = libraryIcon;
-                        }
-                        marker = new window.naver.maps.Marker({
-                            position: new window.naver.maps.LatLng(result[i][j].latitude, result[i][j].longitude),
+            const validResults = result.filter(item => item !== null);
+        
+            validResults.forEach(bookplaces => {
+                bookplaces.forEach(bookplace => {
+                    if (result) {
+                        const { type, name, stock, latitude, longtitude } = bookplace;
+                        console.log(bookplace);
+                        const marker = new window.naver.maps.Marker({
+                            position: new window.naver.maps.LatLng(latitude, longtitude),
                             map: map,
                             icon: {
-                                content: `<img src="${icon}" alt="${result[i][j].name} Marker" style="width:30px; height:30px;">`,
+                                content: `<img src="${aladinIcon}" alt="${name} Marker" style="width:30px; height:30px;">`,
                                 anchor: new window.naver.maps.Point(15, 30),
                             },
                         });
-                        array.push(marker);
-        
-                        marker.addListener('mouseover', () => {
-                            new window.naver.maps.InfoWindow({
-                                content: result[i][j].name,
-                                position: marker.getPosition(),
-                            }).open(map, marker);
-                        });
-        
-                        marker.addListener('mouseout', () => {
-                            map.closeInfoWindow();
-                        });
+                        console.log("마커들 : ",marker)
+                        console.log("마커레프:",markerRef.current)
+                        if (markerRef.current && result){
+                            console.log("마커레프:",markerRef.current)
+                            markerRef.current.push(marker);
+                        }
+                        
+
                     }
-                }
-            }
+                });
+
+            });
         }
-        console.log(marker);
-    }, [result]);
+    }, [map, result]);
     
 
     return (
         <>
-            {loading && <div>Loading...</div>}
+            {/* {bookplace && <div>Loading...</div>} */}
             <MapContainer>
-                {bookplaces.map(bookplace => (
+                {markerRef.current && markerRef.current.length > 0  && markerRef.current.map((marker, index) => (
                     <Marker
-                        key={bookplace.name}
+                        key={index}
+                        position={marker.getPosition()}
+                    >
+                        <Popup>{marker.name}</Popup>
+                    </Marker>
+                ))}
+                {/* {markerRef.current.map((marker, index) => (
+                    
+                    <Marker
+                        key ={bookplace.name}
                         position={[parseFloat(bookplace.latitude), parseFloat(bookplace.longitude)]}
                     >
                         <Popup>{bookplace.name}</Popup>
                     </Marker>
-                ))}
+                ))} */}
             </MapContainer>
             
         </>
